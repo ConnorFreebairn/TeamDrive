6c15638996b190d626f3085d10fb2453c9d29464
Connor Freebairn <ConnorFreebairn@users.noreply.github.com>
2020-06-07 19:28:48 +0100

    7

--- teamdrive.sh
+++ teamdrive.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# apt-get update
+# curl https://rclone.org/install.sh | sudo bash
+
 mkdir /mnt/TeamDrive
 
 curl https://raw.githubusercontent.com/ConnorFreebairn/TeamDrive/master/rclone%40TeamDrive.service --output "/etc/systemd/system/rclone@TeamDrive.service"
