86d227f94caa1ba73911a9fc3cd5e3101005fc5f
Connor Freebairn <ConnorFreebairn@users.noreply.github.com>
2020-06-07 18:34:06 +0100

    Rclone Inital Commit

---
+++ teamdrive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/ConnorFreebairn/TeamDrive/rclone@TeamDrive.service --output "/etc/systemd/system/rclone@TeamDrive.service"
+
+systemctl enable rclone@TeamDrive.service
+
+systemctl start rclone@TeamDrive.service
