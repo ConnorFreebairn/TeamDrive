69ff6af80a679181c7b1fca51a2b02142ca622e0
Connor Freebairn <ConnorFreebairn@users.noreply.github.com>
2020-06-07 18:39:59 +0100

    3

--- teamdrive.sh
+++ teamdrive.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 curl https://raw.githubusercontent.com/ConnorFreebairn/TeamDrive/rclone@TeamDrive.service --output "/etc/systemd/system/rclone@TeamDrive.service"
 
 systemctl enable rclone@TeamDrive.service
