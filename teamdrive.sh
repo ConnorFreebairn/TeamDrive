9aa66d7804badcde8b80c5f34a68343a0b08e923
Connor Freebairn <ConnorFreebairn@users.noreply.github.com>
2020-07-22 23:52:16 +0100

    Update teamdrive.sh

--- teamdrive.sh
+++ teamdrive.sh
@@ -6,6 +6,10 @@
 
 mkdir /mnt/TeamDrive
 
+systemctl stop rclone@TeamDrive.service
+
+systemctl disable rclone@TeamDrive.service
+
 curl https://raw.githubusercontent.com/ConnorFreebairn/TeamDrive/master/rclone%40TeamDrive.service --output "/etc/systemd/system/rclone@TeamDrive.service"
 
 systemctl enable rclone@TeamDrive.service
